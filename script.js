1cb6a152c7085ed9e50f6cc2c6ae674d5a40115e
elaakaraman <elanr.krmn00@gmail.com>
2026-08-05 09:57:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const urunlerAlani = document.getElementById("urunler");
 
+urunlerAlani.innerHTML = `
+    <div class="urun-kart">
+        <h3>💄 BeautyBase'e Hoş Geldiniz</h3>
+        <p>Yakında makyaj ürünleri burada listelenecek.</p>
+    </div>
+`;
