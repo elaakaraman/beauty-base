ad6390ecf3cb58114e1b67c7ee007a762229d081
elaakaraman <elanr.krmn00@gmail.com>
2026-08-05 09:58:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const urunlerAlani = document.getElementById("urunler");
 
-urunlerAlani.innerHTML = `
-    <div class="urun-kart">
-        <h3>💄 BeautyBase'e Hoş Geldiniz</h3>
-        <p>Yakında makyaj ürünleri burada listelenecek.</p>
-    </div>
-`;
+fetch("products.json")
+  .then(response => response.json())
+  .then(urunler => {
+
+    urunler.forEach(urun => {
+
+      urunlerAlani.innerHTML += `
+        <div class="urun-kart">
+          <h2>${urun.urunAdi}</h2>
+
+          <p><b>Marka:</b> ${urun.marka}</p>
+
+          <p><b>Kategori:</b> ${urun.kategori}</p>
+
+          <p><b>Fiyat:</b> ${urun.fiyat} TL</p>
+
+          <p><b>Puan:</b> ⭐ ${urun.puan}</p>
+
+          <p>${urun.aciklama}</p>
+        </div>
+      `;
+
+    });
+
+  });
