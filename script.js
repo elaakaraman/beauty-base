856750c2d05d06ba814d114806b4eaedc0e45f7e
elaakaraman <elanr.krmn00@gmail.com>
2026-08-06 10:02:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const aramaKutusu = document.getElementById("arama");
 
 let tumUrunler = [];
 
-// Ürünleri JSON dosyasından oku
+// JSON dosyasını oku
 fetch("products.json")
     .then(response => response.json())
     .then(data => {
         tumUrunler = data;
         urunleriGoster(tumUrunler);
     })
-    .catch(error => {
-        urunlerAlani.innerHTML = "<h2>Ürünler yüklenemedi.</h2>";
-        console.error(error);
+    .catch(() => {
+        urunlerAlani.innerHTML =
+            "<h2>❌ Ürünler yüklenemedi.</h2>";
     });
 
 // Ürünleri ekrana yazdır
@@ -23,31 +23,33 @@ function urunleriGoster(liste) {
     liste.forEach(urun => {
 
         urunlerAlani.innerHTML += `
-            <div class="urun-kart">
 
-                <img src="${urun.resim}" alt="${urun.urunAdi}">
+        <div class="urun-kart">
 
-                <div class="urun-bilgi">
+            <img src="${urun.resim}" alt="${urun.urunAdi}">
 
-                    <h2>${urun.urunAdi}</h2>
+            <div class="urun-bilgi">
 
-                    <p><strong>Marka:</strong> ${urun.marka}</p>
+                <h2>${urun.urunAdi}</h2>
 
-                    <p><strong>Kategori:</strong> ${urun.kategori}</p>
+                <p><strong>Marka:</strong> ${urun.marka}</p>
 
-                    <p>⭐ ${urun.puan}</p>
+                <p><strong>Kategori:</strong> ${urun.kategori}</p>
 
-                    <p>${urun.aciklama}</p>
+                <p>⭐ ${urun.puan}</p>
 
-                    <p class="fiyat">${urun.fiyat} TL</p>
+                <p>${urun.aciklama}</p>
 
-                    <button class="detay-btn">
-                        Detayları Gör
-                    </button>
+                <p class="fiyat">${urun.fiyat} TL</p>
 
-                </div>
+                <button class="detay-btn">
+                    Detayları Gör
+                </button>
 
             </div>
+
+        </div>
+
         `;
 
     });
@@ -59,16 +61,16 @@ aramaKutusu.addEventListener("input", function () {
 
     const kelime = this.value.toLowerCase();
 
-    const filtrelenmis = tumUrunler.filter(urun => {
+    const filtre = tumUrunler.filter(urun =>
 
-        return (
-            urun.urunAdi.toLowerCase().includes(kelime) ||
-            urun.marka.toLowerCase().includes(kelime) ||
-            urun.kategori.toLowerCase().includes(kelime)
-        );
+        urun.urunAdi.toLowerCase().includes(kelime) ||
 
-    });
+        urun.marka.toLowerCase().includes(kelime) ||
 
-    urunleriGoster(filtrelenmis);
+        urun.kategori.toLowerCase().includes(kelime)
+
+    );
+
+    urunleriGoster(filtre);
 
 });
